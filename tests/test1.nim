3c87d4edc72b8e953fad14668dd0394f2a697ff1
George Lemon <georgelemon@protonmail.com>
2022-06-15 18:12:07 +0300

    add tests
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,32 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
+import unittest, pkginfo
 
-import unittest
+test "when not requires <pkg>":
+    when not requires "toktok":
+        static: assert pkg("toktok") == nil
 
-import pkginfo
-test "can add":
-  check add(5, 5) == 10
+test "when requires <pkg>":
+    when requires "semver":
+        static:
+            assert pkg("semver") != nil
+
+test "when requires <pkg> -- check `version`":
+    when requires "semver":
+        static:
+            assert pkg("semver").getVersion >= v "1.1.1"
+
+test "when requires <pkg> -- check `license`":
+    when requires "semver":
+        static:
+            assert pkg("semver").getLicense == "BSD3"
+
+test "check `nimVersion`":
+    static:
+        assert nimVersion() == v "1.6.4"
+
+test "check `pkg` `version`":
+    static:
+        assert pkg().getVersion >= v "0.1.0"
+
+test "check `pkg` `license`":
+    static:
+        assert pkg().getLicense == "MIT"
